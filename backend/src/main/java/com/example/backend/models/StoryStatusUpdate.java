a3ebe26b636cb8fda96c85ffd644176d22150be4
Yenura <yenura02@gmail.com>
2025-04-27 22:24:05 +0530

    models

---
+++ backend/src/main/java/com/example/backend/models/StoryStatusUpdate.java
@@ -0,0 +1,72 @@
+package com.example.backend.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.Date;
+
+@Document(collection = "workoutStatusUpdates")
+@Getter
+@Setter
+public class StoryStatusUpdate {
+    @Id
+    private String id;
+    private String userId;
+    private Date timestamp;
+    private String title;
+    private String image;
+    private String description;
+    private String exerciseType;
+    private int timeDuration;
+    private String intensity;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+}
